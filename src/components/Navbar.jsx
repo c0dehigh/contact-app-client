86f120d0129af283f748cd6f15673f7e16a637f5
hamed zibandeh <high_dev@outlook.com>
2023-02-05 12:35:13 +0330

    create Navbar componet

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,30 @@
+import SearchContacts from "./contact/SearchContacts";
+import { PURPLE, BACKGROUND } from "../helpers/colors";
+const Navbar = () => {
+  return (
+    <nav
+      className="navbar navbar-dark navbar-expand-sm shadow-lg"
+      style={{
+        backgroundColor: BACKGROUND,
+      }}
+    >
+      <div className="container">
+        <div className="row w-100">
+          <div className="col">
+            <div className="navbar-brand">
+              <i className="fa fa-id-card-o" style={{ color: PURPLE }} />
+              Web application manager{" "}
+              <span style={{ color: PURPLE }}>Contacts</span>
+            </div>
+          </div>
+
+          <div className="col">
+            <SearchContacts />
+          </div>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
