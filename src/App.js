4fe9bbf3120d27c6a9619ed332b5ff6bc22b9812
hamed zibandeh <high_dev@outlook.com>
2023-02-09 14:40:17 +0330

    set state and send props

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
+import { useState } from "react";
 import Navbar from "./components/Navbar";
+import Contacts from "./components/contact/Contacts";
 
 import "./App.css";
 
 function App() {
+  const [loading, setLoading] = useState(true);
+  const [getContacts, setContacts] = useState([]);
+
   return (
     <div className="App">
-      <Navbar/>
+      <Navbar />
+      <Contacts contacts={getContacts} loading={loading} />
     </div>
   );
 }
