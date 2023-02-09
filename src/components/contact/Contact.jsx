71812e97035fe925491fd15d7c344f3a67d1ed51
hamed zibandeh <high_dev@outlook.com>
2023-02-09 14:43:48 +0330

    fix

---
+++ src/components/contact/Contact.jsx
@@ -0,0 +1,52 @@
+import { PURPLE, ORANGE, CYAN, RED, CURRENTLINE } from "../../helpers/colors";
+
+const Contact = () => {
+  return (
+    <div className="col-md-6">
+      <div style={{ backgroundColor: CURRENTLINE }} className="card my-2">
+        <div className="card-body">
+          <div className="row align-items-center d-flex justify-content-around">
+            <div className="col-md-4 col-sm-4">
+              <img
+                src="https://via.placeholder.com/200"
+                alt=""
+                style={{ border: `1px solid ${PURPLE} ` }}
+                className="img-fluid rounded"
+              />
+            </div>
+            <div className="col-md-7 col-sm-7 ">
+              <ul className="list-group">
+                <li className="list-group-item list-group-item-dark">
+                  Name and family :{" "}
+                  <span className="fw-bold ">Hamed zibandeh</span>
+                </li>
+
+                <li className="list-group-item list-group-item-dark">
+                  09390020016 : <span className="fw-bold ">Hamed zibandeh</span>
+                </li>
+
+                <li className="list-group-item list-group-item-dark">
+                  high_dev@outlook.com :{" "}
+                  <span className="fw-bold ">Hamed zibandeh</span>
+                </li>
+              </ul>
+            </div>
+            <div className="col-md-1 col-sm-1 d-flex flex-column align-items-center ">
+              <button className="btn my-1" style={{ backgroundColor: ORANGE }}>
+                <i className="fa fa-pencil"></i>
+              </button>
+              <button className="btn my-1" style={{ backgroundColor: CYAN }}>
+                <i className="fa fa-eye"></i>
+              </button>
+              <button className="btn my-1" style={{ backgroundColor: RED }}>
+                <i className="fa fa-trash"></i>
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Contact;
