8e1cd4373292b30d3e2680ebf459a6dc3c6a255b
hamed zibandeh <high_dev@outlook.com>
2023-02-23 13:04:19 +0330

    added componet

--- src/components/Contacts/AddContact.jsx
+++ src/components/Contacts/AddContact.jsx
@@ -1,7 +1,145 @@
-const AddContact = () => {
+import { Link } from "react-router-dom";
+
+import { Spinner } from "../";
+import { COMMENT, GREEN, PURPLE } from "../../helpers/colors";
+
+const AddContact = ({
+  loading,
+  setContactInfo,
+  contact,
+  groups,
+  createContactForm,
+}) => {
   return (
     <>
-      <h2>Add Contact</h2>
+      {loading ? (
+        <Spinner />
+      ) : (
+        <>
+          <section className="p-3">
+            <img
+              src={require("../../assets/man-taking-note.png")}
+              height="400px"
+              style={{
+                position: "absolute",
+                zIndex: "-1",
+                top: "130px",
+                left: "100px",
+                marginLeft: "37vw",
+                opacity: "50%",
+              }}
+            />
+            <div className="container">
+              <div className="row">
+                <div className="col">
+                  <p
+                    className="h4 fw-bold text-center"
+                    style={{ color: GREEN }}
+                  >
+                    Create new contact
+                  </p>
+                </div>
+              </div>
+              <hr style={{ backgroundColor: GREEN }} />
+              <div className="row mt-5">
+                <div className="col-md-4">
+                  <form onSubmit={createContactForm}>
+                    <div className="mb-2">
+                      <input
+                        name="fullname"
+                        type="text"
+                        value={contact.fullname}
+                        onChange={setContactInfo}
+                        className="form-control"
+                        placeholder=" Fullname"
+                        required={true}
+                      />
+                    </div>
+
+                    <div className="mb-2">
+                      <input
+                        name="photo"
+                        type="text"
+                        value={contact.photo}
+                        onChange={setContactInfo}
+                        className="form-control"
+                        required={true}
+                        placeholder="Img Address "
+                      />
+                    </div>
+                    <div className="mb-2">
+                      <input
+                        name="mobile"
+                        type="number"
+                        value={contact.mobile}
+                        onChange={setContactInfo}
+                        className="form-control"
+                        required={true}
+                        placeholder=" Phonenumber"
+                      />
+                    </div>
+                    <div className="mb-2">
+                      <input
+                        type="email"
+                        name="email"
+                        value={contact.email}
+                        onChange={setContactInfo}
+                        className="form-control"
+                        required={true}
+                        placeholder=" Email address"
+                      />
+                    </div>
+                    <div className="mb-2">
+                      <input
+                        type="text"
+                        name="job"
+                        value={contact.job}
+                        onChange={setContactInfo}
+                        className="form-control"
+                        required={true}
+                        placeholder="job"
+                      />
+                    </div>
+                    <div className="mb-2">
+                      <select
+                        name="group"
+                        value={contact.group}
+                        onChange={setContactInfo}
+                        required={true}
+                        className="form-control"
+                      >
+                        <option value=""> choise group</option>
+
+                        {groups.length > 0 &&
+                          groups.map((group) => (
+                            <option key={group.id} value={group.id}>
+                              {group.name}
+                            </option>
+                          ))}
+                      </select>
+                    </div>
+                    <div className="mx-2">
+                      <input
+                        type="submit"
+                        className="btn"
+                        style={{ backgroundColor: PURPLE }}
+                        value=" create contact"
+                      />
+                      <Link
+                        to={"/contacts"}
+                        className="btn mx-2"
+                        style={{ backgroundColor: COMMENT }}
+                      >
+                        cancel
+                      </Link>
+                    </div>
+                  </form>
+                </div>
+              </div>
+            </div>
+          </section>
+        </>
+      )}
     </>
   );
 };
